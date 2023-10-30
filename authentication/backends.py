aebaf87b8f7068e0847c24a3f52147d3c6b2c990
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-30 08:30:47 +0100

    custom middleware:TokenAuthMiddleware

--- authentication/backends.py
+++ authentication/backends.py
@@ -6,24 +6,30 @@ from django.conf import settings
 
 User = get_user_model()
 
+
 class JWTAuthectication(authentication.BasicAuthentication):
-    
     def authenticate(self, request):
-        auth= authentication.get_authorization_header(request)
-        
+        auth = authentication.get_authorization_header(request)
+
         if not auth:
             return
-        
-        prefix,token = auth.decode('utf-8').split(" ")
-        
+
+        prefix, token = auth.decode("utf-8").split(" ")
+
         try:
-            payload = jwt.decode(token,settings.JWT_SECRET_KEY,algorithms=[settings.ALGORITHM,])
-            user = User.objects.get(email=payload['email'])
-            
-            return (user,token)
-            
+            payload = jwt.decode(
+                token,
+                settings.JWT_SECRET_KEY,
+                algorithms=[
+                    settings.ALGORITHM,
+                ],
+            )
+            user = User.objects.get(email=payload["email"])
+
+            return (user, token)
+
         except jwt.DecodeError:
             raise exceptions.AuthenticationFailed("invalid token")
-        
+
         except jwt.ExpiredSignatureError:
             raise exceptions.AuthenticationFailed("token expired")
---
+++ authentication/middleware.py
@@ -0,0 +1,45 @@
+import jwt
+from channels.middleware import BaseMiddleware
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from channels.db import database_sync_to_async
+
+User = get_user_model()
+
+
+class TokenAuthMiddleware(BaseMiddleware):
+    async def __call__(self, scope, recieve, send):
+        user = await self.get_user(scope, send)
+        print(scope)
+        return await super().__call__(scope, recieve, send)
+
+    async def send_authentication_error(self, send, error):
+        await send(
+            {
+                "type": "websocket.close",
+                "code": 4101,
+                "text": str(error),
+            }
+        )
+
+    async def get_user(self, scope, send):
+        try:
+            token = scope["query_string"].decode("utf8").split("=")[1]
+            payload = jwt.decode(
+                token,
+                settings.JWT_SECRET_KEY,
+                algorithms=[
+                    settings.ALGORITHM,
+                ],
+            )
+            user = await database_sync_to_async(User.objects.get)(
+                email=payload["email"]
+            )
+
+        except (IndexError, jwt.DecodeError):
+            await self.send_authentication_error(send, "Invalid token")
+        except jwt.ExpiredSignatureError:
+            await self.send_authentication_error(send, "token expired")
+        else:
+            return user
