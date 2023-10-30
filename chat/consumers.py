beeca2054a34e0ab37cbbdf0e6564327c11a66e7
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-30 08:51:41 +0100

    restored consumer code

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,47 +9,43 @@ from authentication.models import CustomUser
 class ChatConsumer(AsyncWebsocketConsumer):
     # initial connection between client and server
     async def connect(self):
-        await self.accept()
-        # pass
-        # me = self.scope["user"]
-        # other_username = self.scope["url_route"]["kwargs"]["username"]
-        # other_user = await sync_to_async(CustomUser.objects.get)(
-        #     username=other_username
-        # )
-        # for key, value in self.scope.items():
-        #     print(key, value)
+        me = self.scope["user"]
+        other_username = self.scope["url_route"]["kwargs"]["username"]
+        other_user = await sync_to_async(CustomUser.objects.get)(
+            username=other_username
+        )
+        for key, value in self.scope.items():
+            print(key, value)
 
-        # if other_user:
-        #     self.thread = await sync_to_async(
-        #         Thread.objects.get_or_create_personal_thread
-        #     )(me, other_user)
+        if other_user:
+            self.thread = await sync_to_async(
+                Thread.objects.get_or_create_personal_thread
+            )(me, other_user)
 
-        #     self.room_name = f"personal_thread_{self.thread.id}"
+            self.room_name = f"personal_thread_{self.thread.id}"
 
-        #     await self.channel_layer.group_add(self.room_name, self.channel_name)
-        #     await self.accept()
+            await self.channel_layer.group_add(self.room_name, self.channel_name)
+            await self.accept()
 
     async def receive(self, text_data):
-        pass
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json["message"]
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
 
-        # await self.store_message(message)
+        await self.store_message(message)
 
-        # await self.channel_layer.group_send(
-        #     self.room_name, {"type": "chat_message", "message": message}
-        # )
+        await self.channel_layer.group_send(
+            self.room_name, {"type": "chat_message", "message": message}
+        )
 
-    # async def chat_message(self, event):
-    #     print(event)
-    #     message = event["message"]
+    async def chat_message(self, event):
+        message = event["message"]
 
-    #     await self.send(text_data=json.dumps({"type": "chat", "message": message}))
+        await self.send(text_data=json.dumps({"type": "chat", "message": message}))
 
-    # async def disconnect(self, event):
-    #     await self.channel_layer.group_discard(self.room_name, self.channel_name)
+    async def disconnect(self, event):
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
-    # @database_sync_to_async
-    # def store_message(self, text):
-    #     message = Message(thread=self.thread, sender=self.scope["user"], text=text)
-    #     message.save()
+    @database_sync_to_async
+    def store_message(self, text):
+        message = Message(thread=self.thread, sender=self.scope["user"], text=text)
+        message.save()
